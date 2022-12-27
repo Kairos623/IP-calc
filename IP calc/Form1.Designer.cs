030c0e6ccbbf260c1a4499ab771c6ffb36026d01
Kairos623 <99708498+Kairos623@users.noreply.github.com>
2022-12-27 14:42:52 +0900

    Add files via upload

---
+++ IP calc/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace IP_calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.result = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IPNet = new System.Windows.Forms.Label();
+            this.Mask = new System.Windows.Forms.Label();
+            this.IPClass = new System.Windows.Forms.Label();
+            this.ShirAd = new System.Windows.Forms.Label();
+            this.MaskUnit = new System.Windows.Forms.TextBox();
+            this.firstIP = new System.Windows.Forms.TextBox();
+            this.secondIP = new System.Windows.Forms.TextBox();
+            this.thirdIP = new System.Windows.Forms.TextBox();
+            this.fourthIP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.colhost = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(264, 34);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(75, 23);
+            this.result.TabIndex = 2;
+            this.result.Text = "Посчитать";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP сети";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Маска";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Широковещательный адрес";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Класс";
+            // 
+            // IPNet
+            // 
+            this.IPNet.AutoSize = true;
+            this.IPNet.Location = new System.Drawing.Point(73, 118);
+            this.IPNet.Name = "IPNet";
+            this.IPNet.Size = new System.Drawing.Size(40, 13);
+            this.IPNet.TabIndex = 7;
+            this.IPNet.Text = "0.0.0.0";
+            // 
+            // Mask
+            // 
+            this.Mask.AutoSize = true;
+            this.Mask.Location = new System.Drawing.Point(73, 145);
+            this.Mask.Name = "Mask";
+            this.Mask.Size = new System.Drawing.Size(40, 13);
+            this.Mask.TabIndex = 8;
+            this.Mask.Text = "0.0.0.0";
+            // 
+            // IPClass
+            // 
+            this.IPClass.AutoSize = true;
+            this.IPClass.Location = new System.Drawing.Point(168, 193);
+            this.IPClass.Name = "IPClass";
+            this.IPClass.Size = new System.Drawing.Size(0, 13);
+            this.IPClass.TabIndex = 9;
+            // 
+            // ShirAd
+            // 
+            this.ShirAd.AutoSize = true;
+            this.ShirAd.Location = new System.Drawing.Point(73, 170);
+            this.ShirAd.Name = "ShirAd";
+            this.ShirAd.Size = new System.Drawing.Size(40, 13);
+            this.ShirAd.TabIndex = 10;
+            this.ShirAd.Text = "0.0.0.0";
+            // 
+            // MaskUnit
+            // 
+            this.MaskUnit.Location = new System.Drawing.Point(15, 84);
+            this.MaskUnit.Multiline = true;
+            this.MaskUnit.Name = "MaskUnit";
+            this.MaskUnit.Size = new System.Drawing.Size(67, 20);
+            this.MaskUnit.TabIndex = 1;
+            this.MaskUnit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MaskUnit_KeyPress);
+            // 
+            // firstIP
+            // 
+            this.firstIP.Location = new System.Drawing.Point(15, 36);
+            this.firstIP.Name = "firstIP";
+            this.firstIP.Size = new System.Drawing.Size(46, 20);
+            this.firstIP.TabIndex = 11;
+            this.firstIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.firstIP_KeyPress);
+            // 
+            // secondIP
+            // 
+            this.secondIP.Location = new System.Drawing.Point(67, 36);
+            this.secondIP.Name = "secondIP";
+            this.secondIP.Size = new System.Drawing.Size(46, 20);
+            this.secondIP.TabIndex = 12;
+            this.secondIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.secondIP_KeyPress);
+            // 
+            // thirdIP
+            // 
+            this.thirdIP.Location = new System.Drawing.Point(119, 36);
+            this.thirdIP.Name = "thirdIP";
+            this.thirdIP.Size = new System.Drawing.Size(46, 20);
+            this.thirdIP.TabIndex = 13;
+            this.thirdIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.thirdIP_KeyPress);
+            // 
+            // fourthIP
+            // 
+            this.fourthIP.Location = new System.Drawing.Point(171, 36);
+            this.fourthIP.Name = "fourthIP";
+            this.fourthIP.Size = new System.Drawing.Size(46, 20);
+            this.fourthIP.TabIndex = 14;
+            this.fourthIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.fourthIP_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Введите IP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Введите количество единиц в маске";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Количество хостов";
+            // 
+            // colhost
+            // 
+            this.colhost.AutoSize = true;
+            this.colhost.Location = new System.Drawing.Point(165, 221);
+            this.colhost.Name = "colhost";
+            this.colhost.Size = new System.Drawing.Size(0, 13);
+            this.colhost.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 315);
+            this.Controls.Add(this.colhost);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fourthIP);
+            this.Controls.Add(this.thirdIP);
+            this.Controls.Add(this.secondIP);
+            this.Controls.Add(this.firstIP);
+            this.Controls.Add(this.ShirAd);
+            this.Controls.Add(this.IPClass);
+            this.Controls.Add(this.Mask);
+            this.Controls.Add(this.IPNet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.MaskUnit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label IPNet;
+        private System.Windows.Forms.Label Mask;
+        private System.Windows.Forms.Label IPClass;
+        private System.Windows.Forms.Label ShirAd;
+        private System.Windows.Forms.TextBox MaskUnit;
+        private System.Windows.Forms.TextBox firstIP;
+        private System.Windows.Forms.TextBox secondIP;
+        private System.Windows.Forms.TextBox thirdIP;
+        private System.Windows.Forms.TextBox fourthIP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label colhost;
+    }
+}
+
---
+++ IP calc/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string toDemacial(string binary)
+        {
+            char[] reBinary = binary.ToCharArray();
+            Array.Reverse(reBinary);
+            double demacial = 0;
+            int power = 0;
+            foreach(char s in reBinary)
+            {
+                if (s is '1')
+                {
+                    demacial += Math.Pow(2,power);
+                    power++;
+                    continue;
+                }
+                if (s is '0')
+                {
+                    power++;
+                }
+            }
+            return Convert.ToString(demacial);
+        }
+
+        private string toBinar(int demacial)
+        {
+            int num = demacial;
+            char[] binary = new char[8];
+            int i = 0;
+            while (num >= 1)
+            {
+                if ((num % 2) == 0)
+                {
+                    num = num / 2;
+                    binary[i] = '0';
+                    i++;
+                    continue;
+                }
+                if ((num % 2) != 0)
+                {
+                    num = num / 2;
+                    binary[i] = '1';
+                    i++;
+                    continue;
+                }
+            }
+
+            Array.Reverse(binary);
+            return string.Join("", binary);
+        }
+
+        private string Inverse(string mask)
+        {
+            char[] maskarray = mask.ToCharArray();
+
+            for(int i = 0; i < mask.Length; i++)
+            {
+                if (mask[i] == '1')
+                {
+                    maskarray[i] = '0';
+                    continue;
+                }
+                if (mask[i] == '0')
+                {
+                    maskarray[i] = '1';
+                    continue;
+                }
+            }
+            return string.Join("", maskarray);
+        }
+
+
+        private void result_Click(object sender, EventArgs e)
+        {
+            //Ограничения на ввод
+            if ((firstIP.Text == "") | (secondIP.Text == "") | (thirdIP.Text == "") | (fourthIP.Text == "") | (MaskUnit.Text == ""))
+            {
+                MessageBox.Show("Введите необходимые данные");
+            }
+            else
+            {
+                //Ввод IP по октетам
+                int ip1 = Convert.ToInt32(firstIP.Text);
+                int ip2 = Convert.ToInt32(secondIP.Text);
+                int ip3 = Convert.ToInt32(thirdIP.Text);
+                int ip4 = Convert.ToInt32(fourthIP.Text);
+                //Ввод количества единиц
+                int maskunit = Convert.ToInt32(MaskUnit.Text);
+                //Проверка введенных данных
+                if ((((ip1 < 0) & (ip1 > 255)) | ((ip2 < 0) & (ip2 > 255)) | ((ip3 < 0) & (ip3 > 255)) | ((ip4 < 0) & (ip4 > 255))) | (maskunit > 32))
+                {
+                    MessageBox.Show("Введите корректные данные");
+                }
+                else
+                {
+                    //Опеределение класса адреса
+                    string Class;
+                    if (((ip1 >= 0) & (ip1 <= 127)) & ((ip2 >= 0) & (ip2 <= 255)) & ((ip3 >= 0) & (ip3 <= 255)) & ((ip4 >= 0) & (ip4 <= 255)))
+                    {
+                        Class = "A";
+                        IPClass.Text = Class;
+                        IPNet.Text = Convert.ToString(ip1);
+                    }
+                    else if (((ip1 >= 128) & (ip1 <= 191)) & ((ip2 >= 0) & (ip2 <= 255)) & ((ip3 >= 0) & (ip3 <= 255)) & ((ip4 >= 0) & (ip4 <= 255)))
+                    {
+                        Class = "B";
+                        IPClass.Text = Class;
+                        IPNet.Text = Convert.ToString(ip1) + "." + Convert.ToString(ip2);
+                    }
+                    else if (((ip1 >= 192) & (ip1 <= 223)) & ((ip2 >= 0) & (ip2 <= 255)) & ((ip3 >= 0) & (ip3 <= 255)) & ((ip4 >= 0) & (ip4 <= 255)))
+                    {
+                        Class = "C";
+                        IPClass.Text = Class;
+                        IPNet.Text = Convert.ToString(ip1) + "." + Convert.ToString(ip2) + "." + Convert.ToString(ip3);
+                    }
+                    else if (((ip1 >= 224) & (ip1 <= 239)) & ((ip2 >= 0) & (ip2 <= 255)) & ((ip3 >= 0) & (ip3 <= 255)) & ((ip4 >= 0) & (ip4 <= 255)))
+                    {
+                        Class = "D";
+                        IPClass.Text = Class;
+                    }
+                    else if (((ip1 >= 240) & (ip1 <= 255)) & ((ip2 >= 0) & (ip2 <= 255)) & ((ip3 >= 0) & (ip3 <= 255)) & ((ip4 >= 0) & (ip4 <= 255)))
+                    {
+                        Class = "E";
+                        IPClass.Text = Class;
+                    }
+
+
+                    //Перевод октетов в двоичную систему
+                    string B_ip1 = toBinar(ip1);
+                    string B_ip2 = toBinar(ip2);
+                    string B_ip3 = toBinar(ip3);
+                    string B_ip4 = toBinar(ip4);
+
+                    //Нахождение октетов маски (в двоичке)
+                    string MaskFirstOctBin = "";
+                    for (int i = 1; i <= 8; i++)
+                    {
+                        if (i <= maskunit)
+                        {
+                            MaskFirstOctBin += "1";
+                            continue;
+                        }
+
+                        MaskFirstOctBin += "0";
+
+                    }
+                    string MaskSecondOctBin = "";
+                    for (int i = 1; i <= 8; i++)
+                    {
+                        if (i <= maskunit - 8)
+                        {
+                            MaskSecondOctBin += "1";
+                            continue;
+                        }
+
+                        MaskSecondOctBin += "0";
+
+                    }
+                    string MaskThirdOctBin = "";
+                    for (int i = 1; i <= 8; i++)
+                    {
+                        if (i <= maskunit - 16)
+                        {
+                            MaskThirdOctBin += "1";
+                            continue;
+                        }
+
+                        MaskThirdOctBin += "0";
+
+                    }
+                    string MaskFourthOctBin = "";
+                    for (int i = 1; i <= 8; i++)
+                    {
+                        if (i <= maskunit - 24)
+                        {
+                            MaskFourthOctBin += "1";
+                            continue;
+                        }
+
+                        MaskFourthOctBin += "0";
+
+                    }
+
+                    Mask.Text = 
+                        toDemacial(MaskFirstOctBin) + "." + toDemacial(MaskSecondOctBin) + "." + toDemacial(MaskThirdOctBin) + "." + toDemacial(MaskFourthOctBin);
+
+                    //Нахождение Ip адреса сети
+                    string BinMask =  MaskFirstOctBin + MaskSecondOctBin + MaskThirdOctBin + MaskFourthOctBin;
+                    string BinIp = B_ip1 + B_ip2 + B_ip3 + B_ip4;
+
+                    string ipNetBin1 = "";
+                    for(int i = 0; i < 8; i++)
+                    {
+                        if ((BinMask[i] == '1') && (BinIp[i] == '1'))
+                        {
+                            ipNetBin1 += "1";
+                            continue;
+                        }
+                        ipNetBin1 += "0";
+                    }
+                    string ipNetBin2 = "";
+                    for (int i = 8; i < 16; i++)
+                    {
+                        if ((BinMask[i] == '1') && (BinIp[i] == '1'))
+                        {
+                            ipNetBin2 += "1";
+                            continue;
+                        }
+                        ipNetBin2 += "0";
+                    }
+                    string ipNetBin3 = "";
+                    for (int i = 16; i < 24; i++)
+                    {
+                        if ((BinMask[i] == '1') && (BinIp[i] == '1'))
+                        {
+                            ipNetBin3 += "1";
+                            continue;
+                        }
+                        ipNetBin3 += "0";
+                    }
+                    string ipNetBin4 = "";
+                    for (int i = 24; i < 32; i++)
+                    {
+                        if ((BinMask[i] == '1') && (BinIp[i] == '1'))
+                        {
+                            ipNetBin4 += "1";
+                            continue;
+                        }
+                        ipNetBin4 += "0";
+                    }
+                    IPNet.Text = toDemacial(ipNetBin1) + "." + toDemacial(ipNetBin2) + "." + toDemacial(ipNetBin3) + "." + toDemacial(ipNetBin4);
+
+                    //Определение широковещательного адреса
+                    string reversebinmask = Inverse(BinMask);
+                    string binipnet = ipNetBin1 + ipNetBin2 + ipNetBin3 + ipNetBin4;
+
+                    string broadcast1 = "";
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if ((reversebinmask[i] == '1') | (binipnet[i] == '1'))
+                        {
+                            broadcast1 += "1";
+                            continue;
+                        }
+                        broadcast1 += "0";
+                    }
+                    string broadcast2 = "";
+                    for (int i = 8; i < 16; i++)
+                    {
+                        if (BinMask[i] == BinIp[i])
+                        {
+                            broadcast2 += "1";
+                            continue;
+                        }
+                        broadcast2 += "0";
+                    }
+                    string broadcast3 = "";
+                    for (int i = 16; i < 24; i++)
+                    {
+                        if (BinMask[i] == BinIp[i])
+                        {
+                            broadcast3 += "1";
+                            continue;
+                        }
+                        broadcast3 += "0";
+                    }
+                    string broadcast4 = "";
+                    for (int i = 24; i < 32; i++)
+                    {
+                        if (BinMask[i] == BinIp[i])
+                        {
+                            broadcast4 += "1";
+                            continue;
+                        }
+                        broadcast4 += "0";
+                    }
+
+                    ShirAd.Text = toDemacial(broadcast1) + '.' + toDemacial(broadcast2) + '.' + toDemacial(broadcast3) + '.' + toDemacial(broadcast4);
+
+                    //Определние количества хостов
+                    int numhost = 32 - maskunit;
+
+                    double colhostint = Math.Pow(2, numhost) - 2;
+                    colhost.Text = colhostint.ToString();
+                }
+
+
+            }
+        }
+
+
+        private void MaskUnit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void firstIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void secondIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void thirdIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void fourthIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
